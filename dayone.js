12107e920b4715acff2dc196d2a515fcc30b0c13
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2018-11-28 02:19:45 +0000

    day one (datatypes) completed

---
+++ dayone.js
@@ -0,0 +1,18 @@
+
+    // Declare second integer, double, and String variables.
+ 
+
+    i2 = parseInt(readLine());
+    d2 = parseFloat(readLine());
+    s2 = readLine();
+    // Read and save an integer, double, and String to your variables.
+    
+    // Print the sum of both integer variables on a new line.
+    console.log(i2 + i);
+
+    // Print the sum of the double variables on a new line.
+    sumFloat = d + d2;
+    console.log(sumFloat.toPrecision(2));
+    // Concatenate and print the String variables on a new line
+    console.log(s + s2);
+    // The 's' variable above should be printed first.
