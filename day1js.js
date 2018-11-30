df8151d0deb00951569ce4429286717b24a29852
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2018-11-30 00:25:07 +0000

    completed 10 days of JS, second algorithm Day 1

--- day1js.js
+++ day1js.js
@@ -55,3 +55,44 @@ function getPerimeter(length, width) {
 // ------------------------------------------------------------------------------------------------
 // ----------------------------------------Second algorthim of day 1-------------------------------
 
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+/*
+ * Create the function factorial here
+ */
+
+
+function factorial(n) {
+    for (var i = n - 1; i >= 1; i--) {
+        // We store the value of n at each iteration
+        n = n * i;
+    }
+    return n;
+}
+
+function main() {
+    const n = +(readLine());
+    
+    console.log(factorial(n));
+}
