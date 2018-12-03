99a6c2987d21d0b1b9bd432758993b39e7328f76
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2018-12-03 02:27:16 +0000

    base code for thir algorithm of day1, 10days of javascript

---
+++ 10daysofjavascript/day1js.js
@@ -0,0 +1,142 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Calculate the area of a rectangle.
+*
+*   length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the rectangle's area.
+**/
+function getArea(length, width) {
+    let area;
+    // Write your code here
+    area = length * width
+    return area;
+}
+
+/**
+*   Calculate the perimeter of a rectangle.
+*	
+*	length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the perimeter of a rectangle.
+**/
+function getPerimeter(length, width) {
+    let perimeter;
+    // Write your code here
+    perimeter = 2 * (length + width);
+    return perimeter;
+}
+
+// ------------------------------------------------------------------------------------------------
+// ----------------------------------------Second algorthim of day 1-------------------------------
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+/*
+ * Create the function factorial here
+ */
+
+
+function factorial(n) {
+    for (var i = n - 1; i >= 1; i--) {
+        // We store the value of n at each iteration
+        n = n * i;
+    }
+    return n;
+}
+
+function main() {
+    const n = +(readLine());
+    
+    console.log(factorial(n));
+}
+
+
+// ------------------------------------------------------------------------------------------------
+// ----------------------------------------Third algorthim of day 1-------------------------------
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    
+    // Print the area of the circle:
+    
+    // Print the perimeter of the circle:
+
+    try {    
+        // Attempt to redefine the value of constant variable PI
+        PI = 0;
+        // Attempt to print the value of PI
+        console.log(PI);
+    } catch(error) {
+        console.error("You correctly declared 'PI' as a constant.");
+    }
+}
