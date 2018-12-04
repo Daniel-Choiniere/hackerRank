60d109b406966b1b1f2cb6b983f694e4f7a24b1c
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2018-12-04 01:53:07 +0000

    some functionality towards solving algorithm

--- 30daysofcode/daySix.js
+++ 30daysofcode/daySix.js
@@ -1,7 +1,34 @@
+function alternativeCase(string) {
+    var output = "";
+    for (var i = 0; i < string.length; i++) {
+        if (i % 2 != 0) {
+            output += string[i].toUpperCase();
+        }
+        else {
+            output += string[i].toLowerCase();
+        }
+    }
+    return output;
+}
+
+
 function processData(input) {
     //Enter your code here
-} 
-
+    var oddOutput = "";
+    var evenOutput = "";
+    for (var i = 0; i < input.length; i++) {
+        if (i % 2 != 0) {
+            oddOutput += input[i];
+        } else {
+            if (i % 2 == 0) {
+                evenOutput += input[i];
+            }
+        }
+      
+    }
+    console.log(oddOutput);
+    console.log(evenOutput);
+}
 process.stdin.resume();
 process.stdin.setEncoding("ascii");
 _input = "";
