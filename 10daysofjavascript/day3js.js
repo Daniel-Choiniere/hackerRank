d1b99056b5108667f0c4e4d9f1a0181af84c8f43
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2018-12-11 02:00:09 +0000

    completed algorithm 2, day3 10 days of javascript

--- 10daysofjavascript/day3js.js
+++ 10daysofjavascript/day3js.js
@@ -14,3 +14,22 @@ function getSecondLargest(nums) {
         }
     } return largestNumber - 1;
 }
+
+// Solution to second algorithim of day 3
+
+/*
+ * Complete the reverseString function
+ * Use console.log() to print to stdout.
+ */
+function reverseString(s) {
+    try {
+        if (typeof s !== 'string') throw console.log(s.split("").reverse().join(""));
+    }
+    catch (error) {
+        console.log(error.message);
+        console.log(s);
+    }
+    finally {
+        console.log(s.split("").reverse().join(""));
+    }
+} 
