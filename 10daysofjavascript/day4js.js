8ca2cb96822c288b51747853f4af6820f4110708
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2020-04-11 07:33:37 -0400

    solved the second day4 problem, 10 days of JS. Had to create an object constructor and create a method to calculate perimeter of give shape

--- 10daysofjavascript/day4js.js
+++ 10daysofjavascript/day4js.js
@@ -1,32 +1,46 @@
-// Day 4: Create a Rectangle Object
-const a = +(readLine());
-const b = +(readLine());
-var rec = {
-    length: a,
-    width: b,
-    perimeter: (a + b) * 2,
-    area: a * b,
-};
-
-
-
-// Day 4: Count Object
+// DAY 4 PROBLEM #1
 /*
  * Return a count of the total number of objects 'o' satisfying o.x == o.y.
- * 
+ *
  * Parameter(s):
  * objects: an array of objects with integer properties 'x' and 'y'
  */
 function getCount(objects) {
-    
+  // console.log(objects[0].x)
+  let total = 0;
+  for (let i = 0; i < objects.length; i++) {
+    if (objects[i].x === objects[i].y) {
+      total++;
+    }
+  }
+  return total;
 }
 
-
-
-
-// Day 4: Classes
+// DAY 4 PROBLEM #2
 /*
  * Implement a Polygon class with the following properties:
  * 1. A constructor that takes an array of integer side lengths.
  * 2. A 'perimeter' method that returns the sum of the Polygon's side lengths.
  */
+
+class Polygon {
+  constructor(sides) {
+    this.sides = sides;
+  }
+
+  perimeter() {
+    let total = 0;
+    this.sides.forEach((measurement) => {
+      total += measurement;
+    });
+    return total;
+  }
+}
+
+const rectangle = new Polygon([10, 20, 10, 20]);
+const square = new Polygon([10, 10, 10, 10]);
+const pentagon = new Polygon([10, 20, 30, 40, 43]);
+
+console.log(rectangle.perimeter());
+console.log(square.perimeter());
+console.log(pentagon.perimeter());
