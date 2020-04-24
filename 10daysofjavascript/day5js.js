aeb45067f65fa4275f2e86aa84025f4c7e9f0c2c
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2020-04-24 10:18:10 -0400

    completed day 5, template literals, 10 days of javascript

--- 10daysofjavascript/day5js.js
+++ 10daysofjavascript/day5js.js
@@ -13,3 +13,26 @@ class Square extends Rectangle {
     super(s, s);
   }
 }
+
+// DAY 5: TEMPLATE LITERALS
+/*
+ * Determine the original side lengths and return an array:
+ * - The first element is the length of the shorter side
+ * - The second element is the length of the longer side
+ *
+ * Parameter(s):
+ * literals: The tagged template literal's array of strings.
+ * expressions: The tagged template literal's array of expression values (i.e., [area, perimeter]).
+ */
+function sides(literals, ...expressions) {
+  // console.log(expressions[0])
+  // console.log(expressions[1])
+  let perim = expressions[1];
+  let area = expressions[0];
+  let s1 = (perim + Math.sqrt(perim * perim - 16 * area)) / 4;
+  let s2 = (perim - Math.sqrt(perim * perim - 16 * area)) / 4;
+  // console.log(s2);
+  // console.log(s1);
+
+  return [s2, s1];
+}
